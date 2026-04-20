897ec950d1f050cda46358b0175ac4fe319e40c7
trinojusufh <trinohabibie16@gmail.com>
2026-04-20 18:50:34 +0700

    up

--- components/Layout.js
+++ components/Layout.js
@@ -1,18 +1,6 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-import { Inter, Poppins } from "next/font/google";
 
-export const inter = Inter({
-  subsets: ["latin"],
-  variable: "--font-inter",
-});
-
-export const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["400", "600", "700"],
-  variable: "--font-poppins",
-});
-<body className={`${inter.variable} ${poppins.variable} font-sans`}></body>
 export default function Layout({ children, navbarProps }) {
   return (
     <>
